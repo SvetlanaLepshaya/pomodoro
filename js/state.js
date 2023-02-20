092efb93a7e4155cb334cb2dfa4dc6f840f53625
SvetlanaLepshaya <115588963+SvetlanaLepshaya@users.noreply.github.com>
2023-02-20 20:39:06 +0300

    lesson_1

---
+++ js/state.js
@@ -0,0 +1,9 @@
+const WORK_TIME = 11;
+
+
+
+export const state = {
+    timeLeft: WORK_TIME * 60,
+    isActive: false,
+    timerId: 0,
+}
---
+++ js/timer.js
@@ -0,0 +1,33 @@
+import { alarm } from "./alarm.js";
+import { state } from "./state.js"
+
+const minutesElem = document.querySelector('.time__minutes');
+const secondsElem = document.querySelector('.time__seconds');
+
+const showTime = (seconds) => {
+    minutesElem.textContent = Math.floor(seconds/60);
+    
+    if (Math.floor(seconds/60) < 10) {
+        minutesElem.textContent = '0' + Math.floor(seconds/60);
+    } 
+    secondsElem.textContent = seconds % 60;
+    if (Math.floor(seconds%60) < 10) {
+        secondsElem.textContent = '0' + Math.floor(seconds%60);
+    } 
+}
+
+export const startTimer = () => {
+    state.timeLeft -= 1;
+    
+
+    showTime(state.timeLeft);
+
+    if (state.timeLeft > 0 && state.isActive) {
+       state.timerId = setTimeout(startTimer, 1000);
+    }
+
+    if (state.timeLeft <= 0) {
+        alarm();
+
+    }
+}
