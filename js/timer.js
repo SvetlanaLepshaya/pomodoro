34f7cce13e98612c4eba81006d65235c30e69d0a
SvetlanaLepshaya <115588963+SvetlanaLepshaya@users.noreply.github.com>
2023-02-21 22:38:11 +0300

    lesson_2

--- js/timer.js
+++ js/timer.js
@@ -1,33 +1,53 @@
 import { alarm } from "./alarm.js";
-import { state } from "./state.js"
+import { changeActiveBtn } from "./control.js";
+import { state } from "./state.js";
 
-const minutesElem = document.querySelector('.time__minutes');
-const secondsElem = document.querySelector('.time__seconds');
+const minutesElem = document.querySelector(".time__minutes");
+const secondsElem = document.querySelector(".time__seconds");
 
-const showTime = (seconds) => {
-    minutesElem.textContent = Math.floor(seconds/60);
-    
-    if (Math.floor(seconds/60) < 10) {
-        minutesElem.textContent = '0' + Math.floor(seconds/60);
-    } 
-    secondsElem.textContent = seconds % 60;
-    if (Math.floor(seconds%60) < 10) {
-        secondsElem.textContent = '0' + Math.floor(seconds%60);
-    } 
-}
+export const showTime = (seconds) => {
+  minutesElem.textContent = Math.floor(seconds / 60);
+
+  if (Math.floor(seconds / 60) < 10) {
+    minutesElem.textContent = "0" + Math.floor(seconds / 60);
+  }
+  secondsElem.textContent = seconds % 60;
+  if (Math.floor(seconds % 60) < 10) {
+    secondsElem.textContent = "0" + Math.floor(seconds % 60);
+  }
+};
 
 export const startTimer = () => {
-    state.timeLeft -= 1;
+  state.timeLeft -= 20;
+
+  showTime(state.timeLeft);
+
+  if (state.timeLeft > 0 && state.isActive) {
+    state.timerId = setTimeout(startTimer, 1000);
+  }
+
+  if (state.timeLeft <= 0) {
     
 
-    showTime(state.timeLeft);
+    if (state.status === "work") {
+      
+      state.activeToDo.pomodoro += 1;
 
-    if (state.timeLeft > 0 && state.isActive) {
-       state.timerId = setTimeout(startTimer, 1000);
+      if (state.activeToDo.pomodoro % state.count !== 0) {
+        state.status = "break";
+      } else {
+        state.status = "relax";
+      }
+
+
+    } else {
+      state.status = "work";
     }
+    
+    alarm();
+    state.timeLeft = state[state.status] * 60;
 
-    if (state.timeLeft <= 0) {
-        alarm();
-
-    }
-}
+    changeActiveBtn(state.status);
+    startTimer();
+  }
+};
